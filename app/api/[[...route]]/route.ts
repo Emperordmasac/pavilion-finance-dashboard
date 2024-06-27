7388d6f5649c4976c347d06b09897bc3fbcfa45f
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-06-27 18:05:57 +0100

    feat implement accounts api features

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { HTTPException } from 'hono/http-exception'
+// import { HTTPException } from 'hono/http-exception'
 
 import accounts from './accounts'
 
@@ -8,13 +8,13 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse()
-  }
+// app.onError((err, c) => {
+//   if (err instanceof HTTPException) {
+//     return err.getResponse()
+//   }
 
-  return c.json({ error: 'Internal server error' }, 500)
-})
+//   return c.json({ error: 'Internal server error' }, 500)
+// })
 
 // --
 const routes = app.route('/accounts', accounts)
